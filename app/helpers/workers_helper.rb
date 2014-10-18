3c507282368364c6a72c6f28bdd53861623030f0
Vishnu Gopal <g.vishnu@gmail.com>
2014-10-18 17:12:45 +0530

    Adding workers job correctly

--- app/helpers/workers_helper.rb
+++ app/helpers/workers_helper.rb
@@ -1,2 +1,16 @@
 module WorkersHelper
+  
+  def job_type_options
+    [
+      ['Select One',''],
+      ['Masonry', 'masonry'],
+      ['Electrical and House Wiring', 'electical_and_house_wiring'],
+      ['Welding', 'welding'],
+      ['Carpentry', 'carpentry'],
+      ['Plumbing and Sanitation', 'plumbing_and_sanitation'],
+      ['Barbending', 'barbending'],
+      ['Painting and Decoration', 'painting_and_decoration']
+    ]
+  end
+  
 end
