09d3a48d68ecb50f9c9ae5ef5c8ac11e16125583
Vishnu Gopal <g.vishnu@gmail.com>
2014-10-18 20:04:44 +0530

    Tweaking error display again

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  # Rails scaffold helper to display individual errors
   def formatted_errors_for(object, parameter)
     unless @worker.errors.empty?
       content_tag(:div, :class => 'text-danger col-lg-2') do
