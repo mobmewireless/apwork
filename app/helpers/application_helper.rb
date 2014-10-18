ff26edd2574eda58cb880dc19b3db687efb8a15c
Vishnu Gopal <g.vishnu@gmail.com>
2014-10-18 19:59:17 +0530

    Correcting style errors and validation display errors, moving to a better bootstrap 3 framework

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def formatted_errors_for(object, parameter)
+    unless @worker.errors.empty?
+      content_tag(:div, :class => 'text-danger col-lg-2') do
+        object.errors[parameter].join(" & ")
+      end
+    end
+  end
 end
